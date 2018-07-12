a44b49316c7c83755655335ee946ac385109e6f0
Luis Henrique Paim <=>
2018-07-11 23:51:18 -0300

    Add a class library project and a class

---
+++ ElevatorSurvey.Library/Enums/RegexPattern.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElevatorSurvey
+{
+    public enum RegexPattern
+    {
+        OneDigit,
+        TwoDigits,
+        NoPattern
+    }
+}
---
+++ ElevatorSurvey.Library/InputPatternReader.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace ElevatorSurvey
+{
+    public class InputPatternReader
+    {
+        private const string patternOne = @"\b\d{1}[A-Z]{2}\b";
+        private const string patternTwo = @"\b\d{2}[A-Z]{2}\b";
+
+        public RegexPattern Define(string value)
+        {
+            Regex rgx = new Regex(patternOne, RegexOptions.IgnoreCase);
+            var matches = rgx.Matches(value);
+            
+            if (matches.Count > 0)
+            {
+                return RegexPattern.OneDigit;
+            }
+
+            rgx = new Regex(patternTwo, RegexOptions.IgnoreCase);
+            matches = rgx.Matches(value);
+            if (matches.Count > 0)
+            {
+                return RegexPattern.TwoDigits;
+            }
+
+            return RegexPattern.NoPattern;
+        }
+    }
+}
